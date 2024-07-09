028f946e57fc7f555d92e5ce61e5ecbcb306d79e
Levantis Christos <117188793+LevantisChris@users.noreply.github.com>
2024-07-09 19:54:46 +0300

    DO THE SEARCH APPOINT...

--- front_react/src/components/doctor/doctorComp/SearchAppointments.js
+++ front_react/src/components/doctor/doctorComp/SearchAppointments.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+export default function SearchAppointments({onChange, placeholder}) {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle form submission if needed
+        console.log('Form submitted');
+    };
 
-export default function SearchAppointments(props) {
-    return(
-        <div className={"border w-screen"}>HELLOOOO</div>
+    return (
+        <div className="grid grid-rows-2 border w-screen">
+            {/* Search Bar */}
+            <form onSubmit={handleSubmit} className={"h-min"}>
+                {/* Filters must be inserted here */}
+                <div className="m-2 relative">
+                    <input
+                        type="text"
+                        onChange={onChange}
+                        placeholder={placeholder}
+                        className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-72"
+                    />
+                </div>
+            </form>
+
+            {/* Results */}
+            <div className="m-2">
+                Results will go here
+            </div>
+        </div>
     );
 }
