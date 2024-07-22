14e5e8c39ed466900e7f21a559e00caf1cbca00a
Levantis Christos <117188793+LevantisChris@users.noreply.github.com>
2024-07-22 20:12:20 +0300

    Initialized Back end.

---
+++ back_end/clinicManagment/src/main/java/com/levantis/clinicManagment/ClinicManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.levantis.clinicManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagmentApplication.class, args);
+	}
+
+}
---
+++ back_end/clinicManagment/src/test/java/com/levantis/clinicManagment/ClinicManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levantis.clinicManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
