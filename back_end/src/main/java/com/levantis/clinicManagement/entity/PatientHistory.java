6eed3aaffca0744042f3aa52e6d624dc03e2b8b9
Levantis Christos <117188793+LevantisChris@users.noreply.github.com>
2024-08-24 17:54:51 +0300

    Two entities about registration DONE

--- back_end/src/main/java/com/levantis/clinicManagement/entity/PatientHistory.java
+++ back_end/src/main/java/com/levantis/clinicManagement/entity/PatientHistory.java
@@ -1,6 +1,8 @@
 package com.levantis.clinicManagement.entity;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class PatientHistory {
@@ -14,6 +16,17 @@ public class PatientHistory {
     @JoinColumn(name = "history_patient_id", referencedColumnName = "patient_id", nullable = false)
     private Patient patient;
 
+    @OneToMany(mappedBy = "patientHistory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PatientHistoryRegistration> patientHistoryRegistrations = new ArrayList<>();
+
+    public Integer getHistoryId() {
+        return historyId;
+    }
+
+    public void setHistoryId(Integer historyId) {
+        this.historyId = historyId;
+    }
+
     public Patient getPatient() {
         return patient;
     }
@@ -22,11 +35,21 @@ public class PatientHistory {
         this.patient = patient;
     }
 
-    public Integer getHistoryId() {
-        return historyId;
+    public List<PatientHistoryRegistration> getPatientHistoryRegistrations() {
+        return patientHistoryRegistrations;
     }
 
-    public void setHistoryId(Integer historyId) {
-        this.historyId = historyId;
+    public void setPatientHistoryRegistrations(List<PatientHistoryRegistration> patientHistoryRegistrations) {
+        this.patientHistoryRegistrations = patientHistoryRegistrations;
+    }
+
+    public void addPatientHistoryRegistration(PatientHistoryRegistration registration) {
+        patientHistoryRegistrations.add(registration);
+        registration.setPatientHistory(this);
+    }
+
+    public void removePatientHistoryRegistration(PatientHistoryRegistration registration) {
+        patientHistoryRegistrations.remove(registration);
+        registration.setPatientHistory(null);
     }
 }
---
+++ back_end/src/main/java/com/levantis/clinicManagement/entity/PatientHistoryRegistration.java
@@ -0,0 +1,67 @@
+package com.levantis.clinicManagement.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class PatientHistoryRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "p_History_registration_id")
+    private int patientHistoryRegistrationId;
+
+    @Column(name = "p_History_registration_dateRegister", nullable = false)
+    private Date patientHistoryRegistrationDateRegister;
+
+    @Column(name = "p_History_registration_health_problems", nullable = false)
+    private String patientHistoryRegistrationHealthProblems;
+
+    @Column(name = "p_History_registration_treatment", nullable = false)
+    private String patientHistoryRegistrationTreatment;
+
+    @ManyToOne
+    @JoinColumn(name = "patientHistoryId", referencedColumnName = "history_id", nullable = false)
+    private PatientHistory patientHistory;
+
+    public int getPatientHistoryRegistrationId() {
+        return patientHistoryRegistrationId;
+    }
+
+    public void setPatientHistoryRegistrationId(int patientHistoryRegistrationId) {
+        this.patientHistoryRegistrationId = patientHistoryRegistrationId;
+    }
+
+    public Date getPatientHistoryRegistrationDateRegister() {
+        return patientHistoryRegistrationDateRegister;
+    }
+
+    public void setPatientHistoryRegistrationDateRegister(Date patientHistoryRegistrationDateRegister) {
+        this.patientHistoryRegistrationDateRegister = patientHistoryRegistrationDateRegister;
+    }
+
+    public String getPatientHistoryRegistrationHealthProblems() {
+        return patientHistoryRegistrationHealthProblems;
+    }
+
+    public void setPatientHistoryRegistrationHealthProblems(String patientHistoryRegistrationHealthProblems) {
+        this.patientHistoryRegistrationHealthProblems = patientHistoryRegistrationHealthProblems;
+    }
+
+    public String getPatientHistoryRegistrationTreatment() {
+        return patientHistoryRegistrationTreatment;
+    }
+
+    public void setPatientHistoryRegistrationTreatment(String patientHistoryRegistrationTreatment) {
+        this.patientHistoryRegistrationTreatment = patientHistoryRegistrationTreatment;
+    }
+
+    public PatientHistory getPatientHistory() {
+        return patientHistory;
+    }
+
+    public void setPatientHistory(PatientHistory patientHistory) {
+        this.patientHistory = patientHistory;
+    }
+}
