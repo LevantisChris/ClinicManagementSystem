01912eda8f6161127561de3a754e38edd6acf211
Levantis Christos <117188793+LevantisChris@users.noreply.github.com>
2024-08-29 21:47:02 +0300

    Update CreateHistoryReg.js
    Finish the CreateHistoryReg,when the user selecct an appointment, some info might appear.

--- front_react/src/components/doctor/doctorComp/CreateHistoryReg.js
+++ front_react/src/components/doctor/doctorComp/CreateHistoryReg.js
@@ -1,21 +1,38 @@
 import React, {useState} from 'react'
 import UserService from "../../../services/UserService";
+import {Button} from "@material-tailwind/react";
 
 export default function CreateHistoryReg() {
 
-    const [apppointList, setAppointList] = useState([])
+    const [apppointList, setAppointList] = useState([]);
+    const [selectedAppoint, setSelectedAppoint] = useState("")
+    const [showAppointments, setShowAppointments] = useState(false)
 
     const selectAppointment = async (event) => {
         event.preventDefault();
-        const appointmentsList = await UserService.getCreateAndRespectedAppointments();
-        if (appointmentsList.length !== 0) {
-            console.log(appointmentsList);
-            setAppointList(appointmentsList)
-        } else {
-            console.log("The appointments List is empty");
+
+        if(showAppointments === true) {
+            setAppointList([])
+            setShowAppointments(false)
+        }
+
+        if(showAppointments === false) {
+            const appointmentsList = await UserService.getCreateAndRespectedAppointments();
+            if (appointmentsList.length !== 0) {
+                console.log(appointmentsList);
+                setAppointList(appointmentsList)
+                setShowAppointments(true)
+            } else {
+                console.log("The appointments List is empty");
+            }
         }
     };
 
+
+    function handleAppointClick(appointment) {
+        setSelectedAppoint(appointment.appointmentId)
+        setShowAppointments(false)
+    }
 
     return(
         <div className="flex flex-col h-min w-full p-10 cursor-default">
@@ -34,12 +51,58 @@ export default function CreateHistoryReg() {
                     <span className="material-icons-outlined text-blue-600 mr-3 text-4xl">
                         star_half
                     </span>
-                    <button className={"text-sm p-3 rounded bg-slate-300 to-transparent"}
+                    <button className={
+                        apppointList.length === 0
+                            ? "text-sm p-3 rounded bg-slate-300 to-transparent hover:bg-slate-400 transition ease-linear"
+                            : selectedAppoint === ""
+                                ? "text-sm p-3 rounded bg-red-400 hover:bg-red-600 transition ease-linear"
+                                : "text-sm p-3 rounded bg-green-300"
+                    }
                             onClick={(event) => selectAppointment(event)}
                     >
-                        Select relevant appointment
+                        {apppointList.length === 0 ? "Select relevant appointment" : selectedAppoint === "" ? "Close" : selectedAppoint}
                     </button>
                 </div>
+
+                {showAppointments !== false ? (
+                    <div className="bg-slate-300 mt-5 mb-5 rounded p-2 max-h-72 overflow-auto shadow-2xl">
+                        {apppointList.map(appointment => (
+                            <div
+                                className="grid grid-cols-3 w-full h-full bg-blue-200 mt-2 rounded p-2 cursor-pointer"
+                                onClick={() => handleAppointClick(appointment)}
+                            >
+                                <div className="flex flex-col justify-start">
+                                    {/* Appointment Code */}
+                                    <div className="text-sm font-semibold">
+                                        <span className="material-icons-outlined text-gray-600 mx-2 align-text-top">
+                                            fact_check
+                                        </span>
+                                        {"ID: " + appointment.appointmentId}
+                                    </div>
+                                    {/* Time */}
+                                    <div className="text-sm text-gray-500">
+                                        In {appointment.appointmentDate}
+                                    </div>
+                                    <div className="text-sm text-gray-500">
+                                        From {appointment.appointmentStartTime} to {appointment.appointmentEndTime}
+                                    </div>
+                                    {/* Click to view more info about the appointment */}
+                                    <span className="material-icons-outlined text-gray-600">
+                                        more_horiz
+                                    </span>
+                                </div>
+                                <div className="flex flex-col justify-start">
+                                    {/* Code */}
+                                    <div className="text-sm font-semibold">Reason for the appointment</div>
+                                    <div className="text-sm">
+                                        {appointment.appointmentJustification}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : <></>}
+
 
                 <div className="relative z-0 w-full mb-5 group flex items-center">
                     <span className="material-icons-outlined text-blue-600 mr-3 text-4xl">
@@ -61,61 +124,7 @@ export default function CreateHistoryReg() {
                     />
                 </div>
 
-
-                {/*<div className="relative z-0 w-full mb-5 group flex items-center">*/}
-                {/*    <span className="material-icons-outlined text-blue-600 mr-3 text-4xl">*/}
-                {/*        account_circle*/}
-                {/*    </span>*/}
-                {/*    <input*/}
-                {/*        type="text"*/}
-                {/*        name="floating_AMKA"*/}
-                {/*        id="floating_AMKA"*/}
-                {/*        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"*/}
-                {/*        placeholder="Patient AMKA"*/}
-                {/*        required*/}
-                {/*        value={AMKA}*/}
-                {/*        onChange={(e) => setAMKA(e.target.value)}*/}
-                {/*    />*/}
-                {/*</div>*/}
-
-
-                {/*<div className="relative z-0 w-full mb-5 group">*/}
-
-                {/*    <div className="inline-flex items-center">*/}
-                {/*        <span className="material-icons-outlined text-blue-600 mr-3 text-4xl">*/}
-                {/*            edit_note*/}
-                {/*        </span>*/}
-                {/*        <label className="relative flex items-center p-3 -mt-5 rounded-full cursor-pointer"*/}
-                {/*               htmlFor="description">*/}
-                {/*            <input type="checkbox"*/}
-                {/*                   className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"*/}
-                {/*                   id="description"/>*/}
-                {/*            <span*/}
-                {/*                className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">*/}
-                {/*          <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20"*/}
-                {/*               fill="currentColor"*/}
-                {/*               stroke="currentColor" stroke-width="1">*/}
-                {/*            <path fill-rule="evenodd"*/}
-                {/*                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"*/}
-                {/*                  clip-rule="evenodd"></path>*/}
-                {/*          </svg>*/}
-                {/*        </span>*/}
-                {/*        </label>*/}
-                {/*        <label className="mt-px font-light text-gray-700 cursor-pointer select-none"*/}
-                {/*               htmlFor="description">*/}
-                {/*            <div>*/}
-                {/*                <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">*/}
-                {/*                    Associated it with an appointment*/}
-                {/*                </p>*/}
-                {/*                <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700">*/}
-                {/*                    Some registrations might be done during or after an appointment, the appointment*/}
-                {/*                    will change state to Completed*/}
-                {/*                </p>*/}
-                {/*            </div>*/}
-                {/*        </label>*/}
-                {/*    </div>*/}
-
-                {/*</div>*/}
+                <button className={"bg-blue-400 p-2 text-cyan-50 rounded w-full hover:bg-blue-500 transition ease-linear"}>Submit</button>
 
 
             </form>
