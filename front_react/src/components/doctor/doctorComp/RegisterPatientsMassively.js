a6dcc03a04e02e158ce4dcce6b076c1d63a02308
Levantis Christos <117188793+LevantisChris@users.noreply.github.com>
2024-09-05 20:19:21 +0300

    Update RegisterPatientsMassively.js
    Now is responsive

--- front_react/src/components/doctor/doctorComp/RegisterPatientsMassively.js
+++ front_react/src/components/doctor/doctorComp/RegisterPatientsMassively.js
@@ -93,28 +93,28 @@ export default function RegisterPatientsMassively() {
     return (
         loading ? ( <LoadingApp/>) :
             <div className="flex flex-col h-min w-full p-5">
-                <p className={"font-light text-5xl"}>
+                <p className={"font-light text-3xl sm:text-5xl"}>
                     Register patients massively
                 </p>
-                <p className={"mt-3 font-light text-slate-400"}>
+                <p className={"mt-3 font-light text-slate-400 text-xs sm:text-lg"}>
                     You can use an excel file for doing it, but the file must follow a pattern. Download the file pattern
                     bellow.
                 </p>
 
 
-                <div className={"grid grid-cols-3 mt-3"}>
+                <div className={"grid grid-cols-3 mr-12 sm:mr-0 mt-3"}>
 
-                    <div className="flex items-center justify-center w-full col-span-2">
+                    <div className="flex items-center justify-center w-full  col-span-2">
                         <label htmlFor="dropzone-file"
                                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500">
                             <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                <svg className="w-5 sm:w-8 h-5 sm:h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                           stroke-width="2"
                                           d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                                 </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <p className="mb-2 text-xs sm:text-sm text-gray-500 dark:text-gray-400">
                                     <span className="font-semibold">Click to upload</span> or drag and drop
                                 </p>
                                 <p className="text-xs text-gray-500 dark:text-gray-400">Only Excel files (.xls, .xlsx)</p>
@@ -136,27 +136,28 @@ export default function RegisterPatientsMassively() {
                                           stroke-width="2"
                                           d="M5 9v4a3 3 0 0 0 3 3h4a3 3 0 0 0 3-3V9m-4 4V3m-4 5 4 4 4-4"/>
                                 </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                <p className="mb-2 text-xs text-center sm:text-sm text-gray-500 dark:text-gray-400"><span
                                     className="font-semibold">Click to download the pattern file</span></p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">Download your file here</p>
+                                <p className="text-xs text-gray-500 dark:text-gray-400 text-center">Download your file here</p>
                             </div>
                         </a>
                     </div>
                 </div>
 
                 {TABLE_ROWS.length !== 0 && unsuccedRegistrations.length === 0 ?
-                    <div className={"mt-4"}>
+                    <div className={"mt-4 mr-10 sm:mr-0"}>
                         <div className={"grid grid-cols-2 gap-2"}>
                             <label id="disabled-input-2" aria-label="disabled input 2"
-                                   className={"rounded bg-slate-300 p-2 text-lg text-center"}
+                                   className={"rounded bg-slate-300 p-2 text-sm sm:text-lg text-center"}
                             >
                                     <span className="material-icons-outlined text-gray-600 mx-2 align-text-top">
                                         fact_check
                                     </span>
                                 The data successfully loaded, when you are ready proceed to registration
                             </label>
-                            <Button className={"w-full h-full bg-green-500"} onClick={handleSubmitClick}>Register
-                                patients</Button>
+                            <Button className={"w-full h-full bg-green-500"} onClick={handleSubmitClick}>
+                                Register patients
+                            </Button>
                         </div>
                         <Card className="w-full overflow-y-auto h-80 mt-4">
                             <table className="w-full min-w-max table-auto text-left">
@@ -215,8 +216,8 @@ export default function RegisterPatientsMassively() {
                         </Card>
                     </div>
                     : TABLE_ROWS.length === 0 && unsuccedRegistrations.length !== 0 ?
-                        <div className={"mt-2"}>
-                            <p className={"text-red-600 font-bold text-3xl"}>
+                        <div className={"mr-10 sm:mr-0 mt-2"}>
+                            <p className={"text-red-600 font-bold text-lg sm:text-3xl"}>
                                 The following patients fail to registered, please choose another file
                             </p>
                             <Card className="h-full w-full overflow-scroll mt-4">
